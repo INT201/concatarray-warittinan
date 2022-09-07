e348f5bcab0324f7d953d49f04b6dee9347f65ca
warittinan <warittinan.kh@mail.kmutt.ac.th>
2022-09-07 14:59:35 +0700

    test1

--- concatArray.js
+++ concatArray.js
@@ -2,5 +2,11 @@ const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
   //code here
+  
+  let x=(array1== null ||array1 == undefined) && (array2 == null||array2== undefined)? undefined:
+  (array1== null ||array1 == undefined) ? array2:
+  (array2== null ||array2 == undefined)?array1:
+  array3=array1.concat(array2)
+  return x
 }
 module.exports = concatArray
